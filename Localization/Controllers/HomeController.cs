982a611ee01597b18de54e4919c993d6ac4b1120
Хамелеон <maxali946@gmail.com>
2018-12-13 11:50:39 +0400

    addLocalization

---
+++ Localization/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Localization.Models;
+using System.Globalization;
+using Microsoft.Extensions.Localization;
+
+namespace Localization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStringLocalizer<HomeController> homeLocalizer;
+        private IStringLocalizer<SharedResource> sharedResource;
+
+        public HomeController(IStringLocalizer<HomeController> homeLocalizer, IStringLocalizer<SharedResource> sharedResource)
+        {
+            this.homeLocalizer = homeLocalizer;
+            this.sharedResource = sharedResource;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(string lang = "")
+        {
+            ViewData["Message"] = homeLocalizer["AboutText"];
+
+            //if (!String.IsNullOrEmpty(lang))
+            //{
+            //    CultureInfo.CurrentCulture = new CultureInfo(lang);
+            //    CultureInfo.CurrentUICulture = new CultureInfo(lang);
+            //}
+
+            CultureViewModel model = new CultureViewModel()
+            {
+                Culture = CultureInfo.CurrentCulture.DisplayName,
+                UICulture = CultureInfo.CurrentCulture.DisplayName
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = homeLocalizer["ContactText"];
+
+            ViewData["Hello"] = sharedResource["Hello"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Localization/Models/CultureViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Localization.Models
+{
+    public class CultureViewModel
+    {
+        [Display(Name = "CultureDisplay")]
+        public string Culture { get; set; }
+
+        [Display(Name = "CultureUIDisplay")]
+        public string UICulture { get; set; }
+    }
+}
